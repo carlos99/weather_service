e3e49dd447fb60ce08147882bb7c83a429fee290
Carlos Orellana <carlos@ufgcorp.com>
2017-01-22 22:53:50 -0600

    Created basic seeds just for start testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+l = Location.create(name: "New York City")
+l.recordings.create(temperature: 32, status: "cloudy")
+l.recordings.create(temperature: 34, status: "rainy")
+l.recordings.create(temperature: 30, status: "rainy")
+l.recordings.create(temperature: 28, status: "cloudy")
+l.recordings.create(temperature: 22, status: "sunny")
